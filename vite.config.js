a63bd7e93add83375dd3b26e592e90acbcff54e3
Lucas A. Ouverney <realfakenerd@users.noreply.github.com>
2021-11-24 20:22:47 -0300

    removed some pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { defineConfig } from 'vite';
+import { publish } from 'gh-pages';
+const ghpages = require('gh-pages');
 
 export default defineConfig({
     server: {
         port: 5000,
+        host: true
     },
-    
-    plugins: [svelte()],
+
+    plugins: [
+        svelte(),
+        // @ts-ignore
+        publish('dist', (err) => {
+            console.log("Published to github", err)
+        })
+    ],
 });
